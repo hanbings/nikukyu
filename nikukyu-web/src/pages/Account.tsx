1aa2e664b6ee8c9e16f45bed6393cc963506ad73
hanbings <hanbings@hanbings.io>
2023-12-16 01:13:43 +0800

    ✨ Feat: Add new application submit page.

--- nikukyu-web/src/pages/Account.tsx
+++ nikukyu-web/src/pages/Account.tsx
@@ -1,13 +1,28 @@
-import {Button} from "@nextui-org/react";
+import {
+    Button,
+    Chip,
+    Input,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    Select,
+    SelectedItems,
+    SelectItem,
+    useDisclosure
+} from "@nextui-org/react";
+import {access} from '../types/token.ts'
 
 export function Account() {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
     return (
         <div className="w-screen h-screen flex flex-col gap-3 bg-green-100 p-2 sm:p-12">
             <div className="w-full flex flex-col gap-3 rounded-2xl bg-white p-6">
                 <div>
                     <p className="text-2xl">Yours Account</p>
                     <p className="text-sm text-gray-500">Click card to edit your account</p>
-
                 </div>
                 <div>
 
@@ -29,9 +44,75 @@ export function Account() {
                     <p className="text-sm text-gray-500">Create your own application</p>
                 </div>
                 <div>
-                    <Button className="bg-green-200">Apply a new OAuth Application</Button>
+                    <Button className="bg-green-200" onPress={onOpen}>Apply a new OAuth Application</Button>
                 </div>
             </div>
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">Apply a new OAuth Application</ModalHeader>
+                            <ModalBody>
+                                <Input type="text" label="Application Name"/>
+                                <Input type="text" label="Application Description"/>
+                                <Input type="text" label="Application Homepage"/>
+                                <Input type="text" label="Private Policy"/>
+                                <Input type="text" label="Teams of Use"/>
+                                <Input type="text" label="Theme Color (Using HEX RGB value)"/>
+                                <Select
+                                    items={access}
+                                    label="Apply some access"
+                                    variant="bordered"
+                                    isMultiline={true}
+                                    selectionMode="multiple"
+                                    placeholder="Select a user"
+                                    labelPlacement="outside"
+                                    classNames={{
+                                        trigger: "min-h-unit-12 py-2",
+                                    }}
+                                    renderValue={(items: SelectedItems<{
+                                        access: string,
+                                        description: string,
+                                        isDanger: boolean
+                                    }>) => {
+                                        return (
+                                            <div className="flex flex-wrap gap-2">
+                                                {items.map((item) => (
+                                                    item.data?.isDanger
+                                                        ? <Chip className="bg-red-500"
+                                                                key={item.key}>{item.data?.access}</Chip>
+                                                        : <Chip className="bg-green-200"
+                                                                key={item.key}>{item.data?.access}</Chip>
+                                                ))}
+                                            </div>
+                                        );
+                                    }}
+                                >
+                                    {(item) => (
+                                        <SelectItem key={item.access} textValue={item.access}>
+                                            <div className="flex gap-2 items-center">
+                                                <div className="flex flex-col">
+                                                    <span className="text-small">{item.access}</span>
+                                                    <span
+                                                        className="text-tiny text-default-400">{item.description}</span>
+                                                </div>
+                                            </div>
+                                        </SelectItem>
+                                    )}
+                                </Select>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="danger" variant="light" onPress={onClose}>
+                                    Cancel
+                                </Button>
+                                <Button className="bg-green-200" onPress={onClose}>
+                                    Done
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
         </div>
     )
 }
