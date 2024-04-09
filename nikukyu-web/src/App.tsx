7b863d2ce147a8386c8124ae862a39af9fa04e7e
hanbings <hanbings@hanbings.io>
2024-04-10 01:51:18 +0800

    🔨 Refact: Refactor frontend code.

---
+++ nikukyu-web/src/App.tsx
@@ -0,0 +1,31 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Home} from "./pages/Home.tsx";
+import {Login} from "./pages/Login.tsx";
+import {Authorize} from "./pages/Authorize.tsx";
+import {Account} from "./pages/Account.tsx";
+
+const store = configureStore({
+    reducer: {},
+})
+
+const query = new QueryClient()
+
+const router = createBrowserRouter([
+    { path: "/", element: <Home/> },
+    { path: "/login", element: <Login/> },
+    { path: "/authorize", element: <Authorize/> },
+    { path: "/account", element: <Account/> },
+])
+
+export default function App() {
+    return (
+        <Provider store={store}>
+            <QueryClientProvider client={query}>
+                <RouterProvider router={router}/>
+            </QueryClientProvider>
+        </Provider>
+    )
+}
---
+++ nikukyu-web/src/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    "api": "http://localhost:8080",
+    "debug": true
+}
