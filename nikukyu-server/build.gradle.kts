93fdbe822a7e89f1f85ac2a6d2b77fc186a4e89f
hanbings <hanbings@hanbings.io>
2024-04-12 02:29:44 +0800

    🐛 Fix: Fix build for Java 21 preview.

--- nikukyu-server/build.gradle.kts
+++ nikukyu-server/build.gradle.kts
@@ -9,7 +9,12 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
+
+tasks.withType<JavaCompile>().configureEach { options.compilerArgs.add("--enable-preview") }
+tasks.withType<Test>().configureEach { jvmArgs("--enable-preview") }
+tasks.withType<JavaExec>().configureEach { jvmArgs("--enable-preview") }
 
 configurations {
     compileOnly {
